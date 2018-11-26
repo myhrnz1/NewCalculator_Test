d15ea7a0398a11f216359b16c306c285d3060cc4
myhrnz1 <myhrnz@outlook.com>
2018-11-26 17:13:25 +0100

    add and implement test of start value of firstNumber

--- NewCalculator_Test.java
+++ NewCalculator_Test.java
@@ -1,4 +1,20 @@
 public class NewCalculator_Test {
+  
+  private static NewCalculator calc = new NewCalculator();
+
+  public static boolean testStartValueOfFirstNumber() {
+    double temp = calc.getFirstNumber();
+    if (temp == 0) {
+      return true;
+    }
+    return false;
+  }
+
   public static void main(String[] args) {
+    if (testStartValueOfFirstNumber()) {
+      System.out.println("testStartValueOfFirstNumber() " + "PASSED");
+    } else {
+      System.out.println("testStartValueOfFirstNumber() " + "FAILED");
+    }
   }
 }
